d8d64c0e3ec0139faac7bf77c54bb84c8553359b
jameygleason <james.p.gleason@gmail.com>
2019-03-23 22:07:31 -0400

    webpack dev config for vanilla complete

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Carousel from './rescale-carousel';
+
+// Will initialize with default settings
+const caro = new Carousel({
+  selector: '.carousel',
+  perPage: 3,
+});
+
+export default caro;
